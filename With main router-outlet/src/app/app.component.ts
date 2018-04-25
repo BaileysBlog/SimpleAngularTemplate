487d145c931011a6aff7c3e5b073753dab2cfb4b
Bailey Miller <bmiller@susquehannasoftware.com>
2018-04-25 18:50:29 -0400

    Added some amount of randomness to the side-nav

--- With main router-outlet/src/app/app.component.ts
+++ With main router-outlet/src/app/app.component.ts
@@ -8,9 +8,32 @@ import { UIService } from './_Services/ui.service';
 })
 export class AppComponent
 {
-  
-  constructor(public UI: UIService)
-  { 
 
+  Folders: Array<any> = new Array<any>();
+
+  constructor(public UI: UIService)
+  {
+
+    for (let index = 0; index < this.getRandomArbitrary(50, 100); index++) 
+    {
+      this.Folders.push({
+        name: `Folder ${index + 1}`,
+        description: "A simple folder",
+        date: this.getToday()
+      });
+    }
+
+  }
+
+  getToday(): Date
+  { 
+    var temp = new Date();
+    return new Date(temp.getFullYear(), temp.getMonth(), temp.getDate());
+
+  }
+
+  getRandomArbitrary(min:number, max:number): number
+  {
+    return Math.random() * (max - min) + min;
   }
 }
